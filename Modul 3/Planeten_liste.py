0c681975f44680f2b1ec667bc92b0ea6545d87ba
lorenzo.vettor <lvettor>
2021-10-06 14:04:08 +0200

    Lektion 6.10.2021

---
+++ Modul 3/Planeten_liste.py
@@ -0,0 +1,5 @@
+planeten = ["Mercur", "Venus", "Erde", "Mars", "Jupiter", "Saturn", "Uranus", "Neptun", "Pluto"]
+print("Das Sonnensystem hat insgesamt", len(planeten), "Planeten")
+print(len(planeten))
+for i in range(len(planeten)):
+    print(i, planeten[i])
---
+++ Modul 3/Theorie/Poker_Karten.py
@@ -0,0 +1,7 @@
+farben = ["Pik", "Karo", "Herz", "Kreuz"]
+kartenWerte = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 20]
+symbol = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+
+for j in range(len(farben)):
+    for i in range(len(symbol)):
+        print(farben[j], symbol[i],)
